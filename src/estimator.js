2a456d63e343753d6e9cb39abc00ae3c945527e1
evicoach@gmail.com <evicoach@gmail.com>
2020-04-09 10:36:32 +0100

    Started building the covid-19 estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data)=>{
+
+    let output = {};
+    
+    let impact = {};
+    let severeImpact = {};
+
+    // Copy the input data as property of the output
+    output.data = {...data, region: data['region']};
+
+    impact.currentlyInfected = data['reportedCases'] * 10;
+    severeImpact.currentlyInfected = data['reportedCases'] * 50;
+
+    impact.infectionsByRequestedTime = (timeInDays)=>{
+        // NB: currentlyInfected doubles every 3 days
+    };
+    severeImpact.infectionsByRequestedTime = (timeInDays)=>{
+        // NB: currentlyInfected doubles every 3 days
+    };
+};
+
+const currentlyInfected = (reportedCases, impactLevel)=>{
+    switch(impactLevel.toLowerCase()){
+        case 'impact':
+            return reportedCases * 10;
+            case 'severeimpact':
+                return reportedCases * 50;
+    }
+}
 
 export default covid19ImpactEstimator;
